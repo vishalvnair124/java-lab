7f5e5eb88cc3ed1d8073fbc22b7eb0a1541e2a4a
VISHAL V NAIR <vishalvnair124@gmail.com>
2023-12-02 15:00:51 +0530

    Checkbox added ...

--- demo.java
+++ demo.java
@@ -1,20 +1,34 @@
 import java.awt.*;
 import java.awt.event.*;
 
-class demo extends Frame implements ActionListener{
+class demo extends Frame implements ItemListener {
     demo() {
-     setBounds(100,100,500,500);
-     Button b1 =new Button("vishal");
-     add(b1);
-     b1.addActionListener(this);
+        setBounds(100, 100, 500, 500);
+        Label label =new Label("                                                               ");
+       
+        Checkbox c1 = new Checkbox("hello",false);
+        Checkbox c2 = new Checkbox("hai",!true);
+        add(c1);
+        add(c2);
+         add(label);
+        c1.addItemListener( new ItemListener() {
+         public void itemStateChanged(ItemEvent e) {
+         label.setText("C++ Checkbox: "  + (e.getStateChange()==1?"checked":"unchecked")); 
+     }   
+                
+        });
+        c2.addItemListener(this);
+        setLayout(new FlowLayout());
     }
-    
+
     public static void main(String vidhal[]) {
         System.out.println("hello ");
         new demo().setVisible(true);
 
     }
-    public void actionPerformed(ActionEvent e){
-        System.out.print("hai");
-    }
+
+    public void itemStateChanged(ItemEvent e) {
+       
+     }   
+ 
 }
