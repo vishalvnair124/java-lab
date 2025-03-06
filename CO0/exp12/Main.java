a69ed42cb51fe65b7cc8dc8fd44a0627f8ca13d7
VISHAL V NAIR <vishalvnair124@gmail.com>
2025-03-06 10:44:07 +0530

    co1 complete

--- CO0/exp12/Main.java
+++ CO0/exp12/Main.java
@@ -1,17 +1,24 @@
-public class Main {
-    public static void main(String[] args) {
-        String text = "Hello World";
-        int vowels = 0, consonants = 0;
-        text = text.toLowerCase();
-        
-        for (char ch : text.toCharArray()) {
-            if (ch >= 'a' && ch <= 'z') {
-                if ("aeiou".indexOf(ch) != -1) vowels++;
-                else consonants++;
-            }
-        }
-        
-        System.out.println("Vowels: " + vowels);
-        System.out.println("Consonants: " + consonants);
+class Main {
+
+  public static void main(String[] args) {
+    String line = "this website is aw3som3.";
+    int vowels = 0, consonants = 0, digits = 0, spaces = 0;
+
+    line = line.toLowerCase();
+    for (int i = 0; i < line.length(); ++i) {
+      char ch = line.charAt(i);
+
+      if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+        ++vowels;
+      }
+      else if ((ch >= 'a' && ch <= 'z')) {
+        ++consonants;
+      }
+
     }
+
+    System.out.println("Vowels: " + vowels);
+    System.out.println("Consonants: " + consonants);
+
+  }
 }
